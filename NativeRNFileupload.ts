5d738e2c71df57d7c99a268f3c4c35ed347c8819
ding_chengjie <dingchenjie1@h-partners.com>
2024-07-16 11:45:35 +0800

    fix: 修改upload接口（新加callback参数）
    Signed-off-by: ding_chengjie <dingchenjie1@h-partners.com>

--- NativeRNFileupload.ts
+++ NativeRNFileupload.ts
@@ -2,7 +2,7 @@ import type { TurboModule } from "react-native/Libraries/TurboModule/RCTExport";
 import { TurboModuleRegistry } from "react-native";
 
 export interface Spec extends TurboModule {
-    upload(options: Object): Promise<string>;
+    upload(options: Object, callback: (err: string, result: string)=>void): void;
 }
 
 export default TurboModuleRegistry.getEnforcing<Spec>("RNFileupload"); 
