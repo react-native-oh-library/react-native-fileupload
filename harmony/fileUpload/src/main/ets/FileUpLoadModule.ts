6266d89d7c1d65b192138f1eb9dc34be89f62834
misaka10032 <78400494+ding-chengjie@users.noreply.github.com>
2024-07-17 09:40:40 +0800

    fix: 修改upload接口（新加callback参数） (#8)
    Signed-off-by: ding_chengjie <dingchenjie1@h-partners.com>

--- harmony/fileUpload/src/main/ets/FileUpLoadModule.ts
+++ harmony/fileUpload/src/main/ets/FileUpLoadModule.ts
@@ -25,63 +25,83 @@
 import { TurboModule, TurboModuleContext } from "@rnoh/react-native-openharmony/ts";
 import Logger from './Logger';
 import { TM } from '@rnoh/react-native-openharmony/generated/ts';
-import common from '@ohos.app.ability.common';
 import request from '@ohos.request';
-
+import { BusinessError } from '@ohos.base';
+import { JSON } from '@kit.ArkTS';
 
 export class FileUpLoadModule extends TurboModule implements TM.RNFileupload.Spec{
   constructor(protected ctx: TurboModuleContext) {
     super(ctx);
   }
 
-  upload(options: Object): Promise<string> {
-    let files: Object = options['files'][0];
-    let context = getContext(this) as common.UIAbilityContext;
+  upload(options: Object, callback: (err: string, result: string)=>void): void {
 
-    // 上传任务配置项
-    let uploadConfig = {
+    let filesArray = new Array();
+    for (let index = 0; index < options['files'].length; index++) {
+      let Object = {
+        filename: '',
+        name: '',
+        uri: '',
+        type: ''
+      };
+      let filesItem = options['files'][index];
+      Object.filename = filesItem['filename'];
+      let path = filesItem['filepath'].substring(0, filesItem['filepath'].lastIndexOf('/'));
+      if (path == this.ctx.uiAbilityContext.cacheDir || path == 'file://' + this.ctx.uiAbilityContext.cacheDir) {
+        Object.uri = 'internal://cache/' + filesItem['filename'];
+      } else if (filesItem['filepath'] == 'internal://cache/' + filesItem['filename']) {
+        Object.uri = filesItem['filepath'];
+      }
+      
+      let i = filesItem['filename'].lastIndexOf('.')
+      let type = filesItem['filename'].substring(i + 1);
+      Object.type = filesItem['filetype'] ? filesItem['filetype'] : type;
+      Object.name = filesItem['name'] ? filesItem['name'] : 'file';
+      filesArray.push(Object);
+    }
+    let uploadConfig: request.UploadConfig = {
       url: options['uploadUrl'],
       header: options['headers'],
       method: options['method'],
-      files: [
-        {
-          filename: files['filename'],
-          name: files['name'],
-          uri: 'internal://cache/'+files['filename'],
-          type: files['filetype']
-        }
-      ],
+      files: filesArray,
       data: [
-        { name: 'file', value: '' }
-      ]
+        options['fields']
+      ],
+    };
+
+    let uploadTask: request.UploadTask;
+    let responseMessage: string
+    try {
+      request.uploadFile(this.ctx.uiAbilityContext, uploadConfig).then((data: request.UploadTask) => {
+        uploadTask = data;
+        uploadTask.on('complete', (taskStates: Array<request.TaskState>) => {
+          for (let i = 0; i < taskStates.length; i++) {
+            Logger.info(`FileUpLoad complete taskState: ${JSON.stringify(taskStates)}`);
+            if (taskStates[i].responseCode == 0) {
+              responseMessage = taskStates[i].message;
+            }
+          }
+          callback('',responseMessage !== '' ? responseMessage : 'File uploaded successfully.');
+          removeEvent(uploadTask);
+        });
+        uploadTask.on('fail', (taskStates: Array<request.TaskState>) => {
+          for (let i = 0; i < taskStates.length; i++) {
+            Logger.info(`FileUpLoad fail taskState: ${JSON.stringify(taskStates[i])}`);
+            responseMessage = taskStates[i].message;
+          }
+          callback(responseMessage, '');
+          removeEvent(uploadTask);
+        });
+      }).catch((err: BusinessError) => {
+        Logger.error(`Failed to request the upload. Code: ${err.code}, message: ${err.message}`);
+      });
+    } catch (err) {
+      Logger.error(`FileUpLoad: Invoke uploadFile failed, code is ${err.code}, message is ${err.message}`);
+    callback(err.message,'');
     }
-    // 将本地应用文件上传至网络服务器
-    return new Promise(async (resolve, reject) => {
-      try {
-        request.uploadFile(context, uploadConfig).then((uploadTask) => {
-          uploadTask.on('complete', (taskStates) => {
-            for (let i = 0; i < taskStates.length; i++) {
-              Logger.info(`FileUpLoad complete taskState: ${JSON.stringify(taskStates[i])}`);
-              if (taskStates[i].responseCode == 0) {
-                resolve(taskStates[i].message !== '' ? taskStates[i].message : 'File uploaded successfully.');
-              }
-            }
-          });
-          uploadTask.on('fail', (taskStates) => {
-            for (let i = 0; i < taskStates.length; i++) {
-              Logger.info(`FileUpLoad fail taskState: ${JSON.stringify(taskStates[i])}`);
-              reject(taskStates[i].message);
-            }
-          });
-        })
-          .catch((err) => {
-            Logger.error(`FileUpLoad: Invoke uploadFile failed, code is ${err.code}, message is ${err.message}`);
-            reject(err.message);
-          })
-      } catch (err) {
-        Logger.error(`FileUpLoad: Invoke uploadFile failed, code is ${err.code}, message is ${err.message}`);
-        reject(err.message);
-      }
-    })
+    const removeEvent = (uploadTask) => {
+      uploadTask.off('fail');
+      uploadTask.off('complete');
+    }
   }
 }
